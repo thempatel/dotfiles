6e2f6a3bf9dcfb98d1768291278ca28ab7b42975
Milan Patel <32933108+thempatel@users.noreply.github.com>
2025-12-12 13:27:15 -0500

    chore: sbox link in more claude things

--- sh/sbox-claude.sh
+++ sh/sbox-claude.sh
@@ -1,15 +1,22 @@
 #!/usr/bin/env bash
+#
 
-HOST_CONFIG="$HOME/.config/claude-container"
-mkdir -p "$HOST_CONFIG"
+set -e
+
+HOST_CC_CONFIG="$HOME/.config/claude-container"
+LOCAL_CC_CONFIG="/root/.claude"
+LOCAL_CONFIG="/root/.config"
+
+mkdir -p "$HOST_CC_CONFIG"
 
 podman run -it --rm \
     -v "$(pwd):/workspace" \
-    -v "$HOST_CONFIG:/root/.claude" \
-    -v "$DOTFILES_HOME:/src/dotfiles" \
+    -v "$HOST_CC_CONFIG:/root/.claude" \
     -v "$DOTFILES_HOME/sbox/start.sh:/usr/local/bin/start" \
     -v "$DOTFILES_HOME/sbox/bashrc.sh:/root/.bashrc" \
-    -v "$DOTFILES_HOME/git:/root/.config/git" \
+    -v "$DOTFILES_HOME/git:$LOCAL_CONFIG/git" \
+    -v "$DOTFILES_HOME/claude/commands:$LOCAL_CC_CONFIG/commands" \
+    -v "$DOTFILES_HOME/claude/skills:$LOCAL_CC_CONFIG/skills" \
     -e "CLAUDE_CONFIG_DIR=/root/.claude" \
     -w /workspace \
     node:20 \

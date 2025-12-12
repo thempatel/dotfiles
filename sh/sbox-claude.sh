77f0d5b09bd2337514b1f9a4c43d14668d9315ae
Milan Patel <32933108+thempatel@users.noreply.github.com>
2025-12-12 13:22:52 -0500

    chore: link to bin, mode, link git

--- sh/sbox-claude.sh
+++ sh/sbox-claude.sh
@@ -9,6 +9,7 @@ podman run -it --rm \
     -v "$DOTFILES_HOME:/src/dotfiles" \
     -v "$DOTFILES_HOME/sbox/start.sh:/usr/local/bin/start" \
     -v "$DOTFILES_HOME/sbox/bashrc.sh:/root/.bashrc" \
+    -v "$DOTFILES_HOME/git:/root/.config/git" \
     -e "CLAUDE_CONFIG_DIR=/root/.claude" \
     -w /workspace \
     node:20 \

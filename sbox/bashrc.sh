bb9bd352d6a62873d4153e06116595c44efb1533
Milan Patel <32933108+thempatel@users.noreply.github.com>
2025-12-14 10:15:33 -0500

    feat: sbox claude base image

--- sbox/bashrc.sh
+++ sbox/bashrc.sh
@@ -3,11 +3,3 @@ eval "$(dircolors -b)"
 alias ls='ls $LS_OPTIONS'
 alias ll='ls $LS_OPTIONS -l'
 alias l='ls $LS_OPTIONS -lA'
-
-export PATH="/tools:$PATH"
-
-if [[ -f $HOME/.local/bin/env ]]; then
-    source "$HOME/.local/bin/env"
-fi
-
-export UV_LINK_MODE=copy
--- sh/sbox-claude.sh
+++ sh/sbox-claude.sh
@@ -3,21 +3,24 @@
 
 set -e
 
+IMAGE_NAME="sbox"
+
 HOST_CC_CONFIG="$HOME/.config/claude-container"
 LOCAL_CC_CONFIG="/root/.claude"
 LOCAL_CONFIG="/root/.config"
+
+"$DOTFILES_HOME/sbox/build.sh"
 
 mkdir -p "$HOST_CC_CONFIG"
 
 podman run -it --rm \
     -v "$(pwd):/workspace" \
+    -v "/workspace/.venv" \
     -v "$HOST_CC_CONFIG:/root/.claude" \
-    -v "$DOTFILES_HOME/sbox/start.sh:/usr/local/bin/start" \
-    -v "$DOTFILES_HOME/sbox/bashrc.sh:/root/.bashrc" \
     -v "$DOTFILES_HOME/git:$LOCAL_CONFIG/git" \
     -v "$DOTFILES_HOME/claude/commands:$LOCAL_CC_CONFIG/commands" \
     -v "$DOTFILES_HOME/claude/skills:$LOCAL_CC_CONFIG/skills" \
     -e "CLAUDE_CONFIG_DIR=/root/.claude" \
     -w /workspace \
-    node:20 \
+    "$IMAGE_NAME" \
     bash

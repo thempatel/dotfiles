fedafb37129b0abd1c75b2b28fa7535e5cea55d1
Milan Patel <32933108+thempatel@users.noreply.github.com>
2026-02-05 16:00:15 -0700

    feat: fix quiet, add no-cache

--- sbox/build.sh
+++ sbox/build.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 #
 
-if [[ -n $QUIET ]]; then
-  EXTRA_ARGS='--progress none'
-fi
+EXTRA_ARGS=()
 
-container build $EXTRA_ARGS -t sbox "$(dirname "$0")"
+while getopts "fq" opt; do
+  case $opt in
+    f) EXTRA_ARGS+=(--no-cache) ;;
+    q) EXTRA_ARGS+=(-q) ;;
+    *) echo "Usage: $0 [-f] [-q]" >&2; exit 1 ;;
+  esac
+done
+
+container build "${EXTRA_ARGS[@]}" -t sbox "$(dirname "$0")"

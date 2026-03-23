7853601ff6a73f0dc340bdecbb2b3873d9085d77
Milan Patel <32933108+thempatel@users.noreply.github.com>
2026-03-23 10:45:34 -0400

    feat: cleaner reload!

--- zsh/dot-zsh.d/01-aliases.zsh
+++ zsh/dot-zsh.d/01-aliases.zsh
@@ -1,5 +1,13 @@
-# dotfiles
-alias reload!="exec $SHELL"
+reload!() {
+  # Unset any env vars added after the pre-zsh.d snapshot
+  local -a _env_after=( ${(k)parameters[(R)*export*]} )
+  for key in "${_env_after[@]}"; do
+    (( ${_reload_env_before[(Ie)$key]} )) || unset "$key"
+  done
+  # Restore PATH to its pre-zsh.d state
+  PATH="$_reload_path_before"
+  exec $SHELL -l
+}
 
 # misc
 alias speedtest="networkQuality -v -s"

3370ea98c6973ae7da7fc1967e6d129f30ee1c20
Milan Patel <32933108+thempatel@users.noreply.github.com>
2026-04-05 10:59:44 -0400

    feat: more cleanup

--- nvim/ftplugin/typescript.lua
+++
@@ -1,2 +0,0 @@
-vim.keymap.set("i", "t", require("config.langs.javascript").add_async, { buffer = true })
-vim.keymap.set("n", "<leader>k", require("config.langs.javascript").goto_exported_symbol, { buffer = true })

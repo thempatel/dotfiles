50b4d673fa85077b68a0016ff98b8d5f6c893ba9
Milan Patel <32933108+thempatel@users.noreply.github.com>
2026-04-06 10:52:42 -0400

    feat: treesitter new api

--- nvim/lua/config/plugins/treesitter.lua
+++ nvim/lua/config/plugins/treesitter.lua
@@ -1,50 +1,65 @@
+local ensure_installed = {
+  "c",
+  "lua",
+  "vim",
+  "vimdoc",
+  "query",
+  "typescript",
+  "javascript",
+  "html",
+  "http",
+  "graphql",
+  "css",
+  "json",
+  "yaml",
+  "bash",
+  "markdown",
+  "markdown_inline",
+  "dockerfile",
+  "go",
+  "java",
+  "jsonc",
+  "regex",
+  "scss",
+  "tsx",
+  "ninja",
+  "python",
+  "rust",
+  "rst",
+  "toml",
+}
+
 local M = {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
   build = ":TSUpdate",
-  event = { "BufReadPre", "BufNewFile" },
+  main = "nvim-treesitter",
   dependencies = {
     { "JoosepAlviste/nvim-ts-context-commentstring" },
     "andymass/vim-matchup",
   },
 }
 
-M.opts = {
-  auto_install = true,
-  ensure_installed = {
-    "c",
-    "lua",
-    "vim",
-    "vimdoc",
-    "query",
-    "typescript",
-    "javascript",
-    "html",
-    "http",
-    "graphql",
-    "css",
-    "json",
-    "yaml",
-    "bash",
-    "markdown",
-    "markdown_inline",
-    "dockerfile",
-    "go",
-    "java",
-    "jsonc",
-    "regex",
-    "ruby",
-    "scss",
-    "tsx",
-    "ninja",
-    "python",
-    "rust",
-    "rst",
-    "toml",
-  },
-}
+M.init = function()
+  vim.api.nvim_create_autocmd("FileType", {
+    callback = function()
+      pcall(vim.treesitter.start)
+      vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+    end,
+  })
+end
 
-M.config = function(_, opts)
-  require("nvim-treesitter").setup(opts)
+M.config = function()
+  local already_installed = require("nvim-treesitter.config").get_installed()
+  local to_install = vim.iter(ensure_installed)
+    :filter(function(parser)
+      return not vim.tbl_contains(already_installed, parser)
+    end)
+    :totable()
+  if #to_install > 0 then
+    require("nvim-treesitter").install(to_install)
+  end
+
   require("ts_context_commentstring").setup({
     enable_autocmd = false,
   })

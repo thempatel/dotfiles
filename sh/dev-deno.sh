0edbfe3982c9ec3ebf49a8606011f28a16e66218
Milan Patel <32933108+thempatel@users.noreply.github.com>
2026-04-23 15:30:23 -0400

    feat: dev-deno can find the deno project nearest to script

--- sh/dev-deno.sh
+++ sh/dev-deno.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
-script_path=$(realpath $1)
+script_path=$(realpath "$1")
 shift
-exec deno run -A --config $DOTFILES_HOME/deno.json "$script_path" "$@"
+
+dir=$(dirname "$script_path")
+config=""
+while [[ "$dir" != "/" ]]; do
+  for f in deno.json deno.jsonc; do
+    if [[ -f "$dir/$f" ]]; then
+      config="$dir/$f"
+      break 2
+    fi
+  done
+  dir=$(dirname "$dir")
+done
+
+if [[ -n "$config" ]]; then
+  exec deno run -A --config "$config" "$script_path" "$@"
+else
+  exec deno run -A "$script_path" "$@"
+fi

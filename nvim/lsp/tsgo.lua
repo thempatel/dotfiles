80386ef5fe177adda3e7edfb3c41bbd163451422
Milan Patel <32933108+thempatel@users.noreply.github.com>
2026-06-12 16:09:49 -0400

    chore: fix tsgo/denols registration

--- nvim/lsp/tsgo.lua
+++ nvim/lsp/tsgo.lua
@@ -1,3 +1,13 @@
 return {
   filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact" },
+  root_dir = function(bufnr, on_dir)
+    local fname = vim.api.nvim_buf_get_name(bufnr)
+    -- Deno projects are owned by denols; don't double-attach here.
+    if vim.fs.root(fname, { "deno.json", "deno.jsonc" }) then
+      return
+    end
+    -- Anchor to the project so go-to-definition resolves across files
+    -- instead of running single-file and jumping to type stubs.
+    on_dir(vim.fs.root(fname, { "tsconfig.json", "jsconfig.json", "package.json", ".git" }) or vim.fs.dirname(fname))
+  end,
 }

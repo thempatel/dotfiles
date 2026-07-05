ffff2166a860a182b488f45051f9eb2563ce809d
Milan Patel <32933108+thempatel@users.noreply.github.com>
2026-07-05 14:43:01 -0400

    feat: add a couple more filepath copy options

--- nvim/lua/config/plugins/gitlinker.lua
+++ nvim/lua/config/plugins/gitlinker.lua
@@ -63,5 +63,27 @@ return {
       desc = "Copy path:line",
       mode = { "v" },
     },
+    {
+      "<leader>gyn",
+      function()
+        local name = vim.fn.expand("%:t")
+        vim.fn.setreg("+", name)
+        vim.notify(name, vim.log.levels.INFO, { title = "Copied" })
+      end,
+      desc = "Copy filename",
+      mode = { "n" },
+    },
+    {
+      "<leader>gyr",
+      function()
+        local abs = vim.fn.expand("%:p")
+        local root = vim.fs.root(0, ".git")
+        local ref = (root and vim.startswith(abs, root)) and abs:sub(#root + 2) or abs
+        vim.fn.setreg("+", ref)
+        vim.notify(ref, vim.log.levels.INFO, { title = "Copied" })
+      end,
+      desc = "Copy repo-relative path",
+      mode = { "n" },
+    },
   },
 }
